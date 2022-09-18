0239c6ba8552d1f9d849f6233fcd3b9d7077f159
mmkuznecov <mmkuznecov2002@gmail.com>
2022-09-18 18:24:36 +0300

    implemented working version of CountVectorizer

---
+++ PYTHON/PYTHON_HW_2/count_vectorizer.py
@@ -0,0 +1,44 @@
+
+class CountVectorizer:
+    """ CountVectorizer class implementation """
+    def __init__(self, lowercase=True):
+
+        self.lowercase = lowercase
+        self.unique_words = None
+
+    def fit_transform(self, corpus):
+        '''Function returns count matrix'''
+
+        if self.lowercase:
+            corpus = [doc.lower() for doc in corpus]
+
+        # create a set of unique words - we will also use them in get_feature_names() method
+
+        unique_words = set()
+        for doc in corpus:
+            unique_words.update(doc.split())
+
+        unique_words = list(unique_words)
+
+        unique_words.sort()  # for reproducibility
+
+        self.unique_words = unique_words
+
+        count_matrix = []
+
+        for doc in corpus:
+
+            doc_dict = {word: 0 for word in self.unique_words}
+            for word in doc.split():
+                if word in doc_dict:
+                    doc_dict[word] += 1
+                else:
+                    doc_dict[word] = 1
+
+            count_matrix.append(list(doc_dict.values()))
+
+        return count_matrix
+
+    def get_feature_names(self):
+        '''Function returns list of unique words'''
+        return self.unique_words
---
+++ PYTHON/PYTHON_HW_2/example_of_use.py
@@ -0,0 +1,41 @@
+from count_vectorizer import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer as SklearnCountVectorizer
+
+corpus = [
+    'Crock Pot Pasta Never boil pasta again',
+    'Pasta Pomodoro Fresh ingredients Parmesan to taste'
+]
+
+vectorizer = CountVectorizer()
+count_matrix = vectorizer.fit_transform(corpus)
+
+
+print("Count matrix:")
+print(count_matrix)
+
+print("Feature names:")
+print(vectorizer.get_feature_names())
+
+# Let's compare the results with sklearn's CountVectorizer for fun
+
+skl_count_vectorizer = SklearnCountVectorizer()
+skl_count_matrix = skl_count_vectorizer.fit_transform(corpus)
+
+print("Sklearn count matrix:")
+print(skl_count_matrix.toarray())
+
+print("Sklearn feature names:")
+print(skl_count_vectorizer.get_feature_names())
+
+'''
+Output:
+Count matrix:
+[[1, 1, 1, 0, 0, 1, 0, 2, 0, 1, 0, 0], [0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1]]
+Feature names:
+['again', 'boil', 'crock', 'fresh', 'ingredients', 'never', 'parmesan', 'pasta', 'pomodoro', 'pot', 'taste', 'to']
+Sklearn count matrix:
+[[1 1 1 0 0 1 0 2 0 1 0 0]
+ [0 0 0 1 1 0 1 1 1 0 1 1]]
+Sklearn feature names:
+['again', 'boil', 'crock', 'fresh', 'ingredients', 'never', 'parmesan', 'pasta', 'pomodoro', 'pot', 'taste', 'to']
+'''
